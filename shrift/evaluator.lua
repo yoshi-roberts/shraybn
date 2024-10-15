eae37721fa898744709e2ef70c64e47b58e26b2d
Moncii <yoshiroberts@proton.me>
2024-10-15 18:41:40 +0000

    Shrift now evaluates boolean literals.

--- shrift/evaluator.lua
+++ shrift/evaluator.lua
@@ -4,6 +4,11 @@ local object = require("shrift.object") --[[@as object]]
 ---@class evaluator
 local evaluator = {}
 
+---@type BooleanObject
+local TRUE = object.Boolean(true)
+---@type BooleanObject
+local FALSE = object.Boolean(false)
+
 ---@param node ASTNode
 ---@return ObjectInterface?
 function evaluator:eval(node)
@@ -16,6 +21,9 @@ function evaluator:eval(node)
 	elseif node:is(ast.IntegerLiteral) then
 		---@cast node ASTIntegerLiteral
 		return object.Integer(node.value)
+	elseif node:is(ast.Boolean) then
+		---@cast node ASTBoolean
+		return self:native_bool_to_boolean_obj(node.value)
 	end
 
 	return nil
@@ -32,6 +40,16 @@ function evaluator:eval_statements(statements)
 	end
 
 	return result
+end
+
+---@param input boolean
+---@return BooleanObject
+function evaluator:native_bool_to_boolean_obj(input)
+	if input then
+		return TRUE
+	end
+
+	return FALSE
 end
 
 return evaluator
