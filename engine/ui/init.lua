65f1f8e4ccf963c9937233f16ccb96fd36773223
Moncii <yoshiroberts@proton.me>
2025-03-17 19:14:33 +0000

    Button elements can now have callbacks defined.

--- engine/ui/init.lua
+++ engine/ui/init.lua
@@ -1,55 +1,36 @@
 local event = require("engine.event")
+local input = require("engine.input")
 local engine = require("engine")
+
+---@alias engine.ui.Element {x: number, y: number, w: number, h: number}
 
 ---@class engine.ui
 local ui = {
 	theme = {},
-	frame = {
-		x = 0,
-		y = 0,
-		w = 0,
-		h = 0,
-	},
-	last = {
-		x = 0,
-		y = 0,
-		w = 0,
-		h = 0,
-	},
-	next = {
-		x = 0,
-		y = 0,
-		w = 0,
-		h = 0,
-	},
+	frame = {}, ---@type engine.ui.Element
+	last = {}, ---@type engine.ui.Element
+	next = {}, ---@type engine.ui.Element
+	mouse_x = 0,
+	mouse_y = 0,
+	focussed = nil,
+	callback = nil, ---@type function
 	is_same_line = false,
 }
 
----@type event.callback
-local function update_ui_input(code, data)
-	local mpos = engine.camera:get_mouse_position()
+function ui:init() end
 
-	if code == event.code.MOUSE_PRESS then
+function ui:update(dt, mx, my)
+	self.mouse_x = mx
+	self.mouse_y = my
+
+	if self.focussed then
+		if self.callback and input.button_pressed(input.mouse_button.LEFT) then
+			self.callback()
+		end
 	end
 
-	if code == event.code.MOUSE_RELEASE then
-	end
-
-	if code == event.code.KEY_PRESS then
-	end
-
-	if code == event.code.KEY_RELEASE then
-	end
-
-	return false
-end
-
-function ui:init()
-	event.register_category(event.category.INPUT, update_ui_input)
-end
-
-function ui:update(dt)
-	local mpos = engine.camera:get_mouse_position()
+	self.focussed = nil
+	self.callback = nil
 end
 
 ---@param theme table
@@ -108,6 +89,17 @@ function ui:update_last(x, y, w, h)
 	self.last.h = h
 end
 
+---@return boolean
+function ui:mouse_in_rect(x, y, w, h)
+	if self.mouse_x >= x and self.mouse_x <= x + w then
+		if self.mouse_y >= y and self.mouse_y <= y + h then
+			return true
+		end
+	end
+
+	return false
+end
+
 function ui:start(x, y, w, h)
 	self.frame.x = x
 	self.frame.y = y
@@ -137,29 +129,36 @@ function ui:label(text)
 	love.graphics.setColor(self.theme.fg_color)
 	love.graphics.printf(text, x, y, w, "left")
 
-	-- local width, wrapped_text = fnt:getWrap(text, self.last.w)
-	-- self.last.h = fnt:getHeight() * #wrapped_text
-	-- self.last.y = y + self.last.h
-	-- self.last.x = x
-
-	-- self:update_last(x, y + self.last.h, w, h)
 	self:set_next_pos(x, y, w, h)
 	self:draw_debug(x, y, w, h)
 end
 
-function ui:button(text)
+---@param text string
+---@param callback function?
+function ui:button(text, callback)
 	local fnt = love.graphics.getFont()
+	local theme = self.theme.button.normal
 
 	local x, y = self:get_next_pos()
 	local w = fnt:getWidth(text)
 	local h = fnt:getHeight()
 
-	love.graphics.setColor(self.theme.outline_color)
+	if input.button_pressed(input.mouse_button.ANY) then
+		print("Hey!")
+	end
+
+	if self:mouse_in_rect(x, y, w, h) then
+		theme = self.theme.button.focussed
+		self.focussed = self.last
+		self.callback = callback and callback or self.callback
+	end
+
+	love.graphics.setColor(theme.outline_color)
 	love.graphics.rectangle("fill", x - 1, y - 1, w + 2, h + 2)
-	love.graphics.setColor(self.theme.bg_color)
+	love.graphics.setColor(theme.bg_color)
 	love.graphics.rectangle("fill", x, y, w, h)
 
-	love.graphics.setColor(self.theme.fg_color)
+	love.graphics.setColor(theme.fg_color)
 	love.graphics.print(text, x, y)
 
 	self:set_next_pos(x, y, w, h)
--- ui_test_layer.lua
+++ ui_test_layer.lua
@@ -1,4 +1,6 @@
 local ui = require("engine.ui")
+local engine = require("engine")
+local input = require("engine.input")
 
 local function ui_attach()
 	ui:init()
@@ -7,13 +9,26 @@ local function ui_attach()
 		fg_color = { 1, 1, 1, 1 },
 		outline_color = { 1, 1, 1, 1 },
 		padding = 4,
+		button = {
+			normal = {
+				bg_color = { 0, 0, 0, 1 },
+				fg_color = { 1, 1, 1, 1 },
+				outline_color = { 1, 1, 1, 1 },
+			},
+			focussed = {
+				bg_color = { 1, 1, 1, 1 },
+				fg_color = { 0, 0, 0, 1 },
+				outline_color = { 1, 1, 1, 1 },
+			},
+		},
 	})
 end
 
 local function ui_detach() end
 
 local function ui_update(dt)
-	ui:update(dt)
+	local mpos = engine.camera:get_mouse_position()
+	ui:update(dt, mpos.x, mpos.y)
 end
 
 local function ui_draw()
@@ -21,7 +36,9 @@ local function ui_draw()
 	ui:label("Hey there!")
 	ui:label("This is a longer line of text.")
 	ui:label("Hey there!")
-	ui:button("Button")
+	ui:button("Button", function()
+		print("Button 1!")
+	end)
 	ui:same_line()
 	ui:button("Button")
 	ui:label("More text.")
